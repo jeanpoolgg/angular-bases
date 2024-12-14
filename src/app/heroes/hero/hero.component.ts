127e109a0abe448b477d228c005833d5630d9e3f
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-14 14:05:05 -0500

    One way data binding - enlazado en una sola vía

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -9,5 +9,11 @@ export class HeroComponent {
   public name: string = 'Ironman';
   public age: number = 45;
 
-  
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${ this.name } - ${ this.age }`;
+  }
 }
