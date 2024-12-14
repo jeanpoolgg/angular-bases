7abf211325a7833d7f5ec48e708e3785e53f94ea
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-14 10:08:53 -0500

    Funcionalidad del contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'bases';
-  public counter: number = 10;
-
-  increaseBy(value: number): void {
-    this.counter += value;
-  }
-
-  resetCounter(): void {
-    this.counter = 10;
-  }
 }
