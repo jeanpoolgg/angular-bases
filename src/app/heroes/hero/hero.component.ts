495e1896f78047a4ccc829e5aaffee07ff96509a
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-14 10:37:25 -0500

    Interpolación, estructura HTML y estilos

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name: string = 'Ironman';
+  public age: number = 45;
 
+  
 }
